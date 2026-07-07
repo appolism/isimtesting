88f25cfd948c00f7072164e93aa3b49e0b23a8a8
appolism <appolisms@gmail.com>
2026-07-07 20:34:05 +0000

    Position of the robot now updates every 45 frames, the addition of a radio tower is very simple to implement so that is not an issue, next work will be on connecting sionna to this all

--- sim_to_flat.py
+++ sim_to_flat.py
@@ -1,23 +1,109 @@
 # Flat File is named 'sim_to_flat.txt'
 import omni.usd
+import omni.timeline
+import omni.kit.app
 from pxr import UsdGeom
+from isaacsim.core.experimental.utils import xform
+
+try:
+	subscription.unsubscribe()
+except:
+	pass
 
 #variables
-stage = omni.usd.get_context().get_stage()
-prim = stage.GetPrimAtPath("/World/jetbot")
+#robot_pos, robot_orientation = xform.get_world_pose("/World/jetbot/chassis")
+#robot_pos_np = robot_pos.numpy()
+app = omni.kit.app.get_app()
+frame_count = 0
+robot_pos_np = [0, 0, 0]
 
-xform = UsdGeom.Xformable(prim)
-matrix = xform.ComputeLocalToWorldTransform(0)
-position = matrix.ExtractTranslation()
-
-pos_list = list(position)
+#functions
+def robot_pos_update(event):
+	global frame_count
+	global robot_pos_np
+	timeline = omni.timeline.get_timeline_interface()
+	if not timeline.is_playing():
+		return
+		
+	frame_count += 1
+	
+	if frame_count % 45 != 0:
+		return
+	
+	robot_pos, robot_orientation = xform.get_world_pose("/World/jetbot/chassis")
+	robot_pos_np = robot_pos.numpy()
+	with open("sim_to_flat.txt", "w") as f:
+		f.write(f"Position(x, y, z): {robot_pos_np[0]}, {robot_pos_np[1]}, {robot_pos_np[2]}")
+	
+	with open("sim_to_flat.txt") as f:
+		print(f.read())
 
 #code
-with open("sim_to_flat.txt", "w") as f:
-	f.write(f"Position(x, y, z): {pos_list[0]}, {pos_list[1]}, {pos_list[2]}")
+subscription = app.get_update_event_stream().create_subscription_to_pop( robot_pos_update )
+
+#with open("sim_to_flat.txt", "w") as f:
+	#f.write(f"Position(x, y, z): {robot_pos_np[0]}, {robot_pos_np[1]}, {robot_pos_np[2]}")
 	
-with open("sim_to_flat.txt") as f:
-	print(f.read())
+#with open("sim_to_flat.txt") as f:
+	#print(f.read())
+	
+#print("lalala")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
